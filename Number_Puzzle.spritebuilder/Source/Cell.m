6f941085b8b3bab6f6fc5a1f6e94c1eebdd60f85
Keenwby <baiyangw@andrew.cmu.edu>
2015-04-28 19:11:46 -0700

    Final_Version2.0

--- Number_Puzzle.spritebuilder/Source/Cell.m
+++ Number_Puzzle.spritebuilder/Source/Cell.m
@@ -14,13 +14,8 @@
     CCLabelTTF *_valueLabel;
 }
 
-<<<<<<< HEAD
 static const NSInteger MAX_NUM = 24;
 static int Fibonacci[MAX_NUM];
-=======
-static const NSInteger WIN_NUM = 25;
-static int Fibonacci[WIN_NUM];
->>>>>>> origin/master
 int num = -1;
 - (void)didLoadFromCCB {
     [self initFibonacci];
--- Number_Puzzle.spritebuilder/Source/Grid.h
+++ Number_Puzzle.spritebuilder/Source/Grid.h
@@ -10,7 +10,6 @@
 
 @interface Grid : CCNodeColor
 
-<<<<<<< HEAD
 @property (nonatomic, assign) NSInteger score;//Step count
 @property (nonatomic, assign) NSInteger winscore;//WinScore
 @property (nonatomic, assign) NSInteger timeleft;//Timer max
@@ -19,16 +18,6 @@
 @property NSInteger maxvalue;//Max value in gird
 @property NSInteger livecells;//Live cells count
 @property NSInteger lastvalue;//Last value
-=======
-@property (nonatomic, assign) NSInteger score;
-@property (nonatomic, assign) NSInteger winscore;
-@property (nonatomic, assign) NSInteger timeleft;
-@property int n ;
-@property int second;
-@property NSInteger maxvalue;
-@property NSInteger livecells;
-@property NSInteger lastvalue;
->>>>>>> origin/master
 
 - (void) setNum:(NSInteger)Value;
 
